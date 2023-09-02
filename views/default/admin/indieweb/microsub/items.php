f17d2398b4bfc3c44ecb3bdde1d9d1e007d1b32a
Nikolai Shcherbin <hello@wzm.me>
2023-09-03 02:49:28 +0300

    Fixed the pagination URL

--- views/default/admin/indieweb/microsub/items.php
+++ views/default/admin/indieweb/microsub/items.php
@@ -37,7 +37,9 @@ $count = elgg_get_entities($options);
 
 if (!empty($count)) {
 	echo elgg_view('navigation/pagination', [
-		'base_url' => elgg_normalize_url('admin/indieweb/microsub/items'),
+		'base_url' => elgg_http_add_url_query_elements(elgg_normalize_url('admin/indieweb/microsub/items'), [
+			'guid' => $entity->guid,
+		]),
 		'offset' => $offset,
 		'count' => $count,
 		'limit' => elgg_get_config('default_limit'),
